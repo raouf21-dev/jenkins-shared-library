5b78ed2f85135a55ebdaa5ef86b325966234cb0b
PDW GROUP <support@PDWs-Air.home>
2025-02-18 17:57:58 +0000

    update image version

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call(){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: "docker-hub-creds", passwordVariable: "PASS", usernameVariable: "USER")]){
-        sh "docker build -t santana20095/demo-app:jma-2.0 ."
+        sh "docker build -t santana20095/demo-app:jma-2.1 ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push santana20095/demo-app:jma-2.0"
+        sh "docker push santana20095/demo-app:jma-2.1"
     }
 }
